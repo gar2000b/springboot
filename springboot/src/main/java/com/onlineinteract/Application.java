242d3c04edeb997740fccc5481574f3dcda1643e
Gary Black <gar2000b@yahoo.com>
2016-04-07 20:07:28 -0400

    committing first basic app with jersey rest + static support

---
+++ springboot/src/main/java/com/onlineinteract/Application.java
@@ -0,0 +1,13 @@
+package com.onlineinteract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}
